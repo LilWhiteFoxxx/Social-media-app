d5f679895ea8aacf127ab1bc8b467d7421a39bf2
LilWhiteFoxxx <bxb.ros1102@gmail.com>
2024-05-03 16:15:01 +0700

    Register & Login

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import multer from 'multer'
 import authRoutes from './routes/auth.js'
 import commentRoutes from './routes/comment.js'
 import likeRoutes from './routes/likes.js'
@@ -10,9 +11,31 @@ import userRoutes from './routes/users.js'
 const app = express()
 
 // middlewares
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true)
+    next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+  })
+
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    const file = req.file
+    res.status(200).json(file.filename)
+})
 
 app.use('/api/auth', authRoutes)
 app.use('/api/comments', commentRoutes)
