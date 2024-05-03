8c79d31ecb93ee2434ee32a63904f83f34d8f095
LilWhiteFoxxx <bxb.ros1102@gmail.com>
2024-05-03 23:57:54 +0700

    Like & Coment

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,59 +1,106 @@
-import "./post.scss";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
-import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Link } from "react-router-dom";
-import Comments from "../comments/Comments";
-import { useState } from "react";
+import './post.scss';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Link } from 'react-router-dom';
+import Comments from '../comments/Comments';
+import { useContext, useState } from 'react';
+import moment from 'moment';
+import { makeRequest } from '../../axios';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { AuthContext } from '../../context/authContext';
 
 const Post = ({ post }) => {
-  const [commentOpen, setCommentOpen] = useState(false);
+    const [commentOpen, setCommentOpen] = useState(false);
 
-  //TEMPORARY
-  const liked = false;
+    const { currentUser } = useContext(AuthContext);
 
-  return (
-    <div className="post">
-      <div className="container">
-        <div className="user">
-          <div className="userInfo">
-            <img src={post.profilePic} alt="" />
-            <div className="details">
-              <Link
-                to={`/profile/${post.userId}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                <span className="name">{post.name}</span>
-              </Link>
-              <span className="date">1 min ago</span>
+    const { isPending, error, data } = useQuery({
+        queryKey: ['likes', post.id],
+        queryFn: () =>
+            makeRequest.get('/likes?postId=' + post.id).then((res) => {
+                return res.data;
+            }),
+    });
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: (liked) => {
+            if (liked) return makeRequest.delete('/likes?postId=' + post.id);
+            return makeRequest.post('/likes', { postId: post.id });
+        },
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['likes'] });
+        },
+    });
+
+    const handleLike = () => {
+        mutation.mutate(data.includes(currentUser.id));
+    };
+
+    return (
+        <div className="post">
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link
+                                to={`/profile/${post.userId}`}
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'inherit',
+                                }}
+                            >
+                                <span className="name">{post.name}</span>
+                            </Link>
+                            <span className="date">
+                                {moment(post.createdAt).fromNow()}
+                            </span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon />
+                </div>
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={'/upload/' + post.img} alt="" />
+                </div>
+                <div className="info">
+                    <div className="item">
+                        {error ? (
+                            'Something went wrong'
+                        ) : isPending ? (
+                            'Loading ...'
+                        ) : data.includes(currentUser.id) ? (
+                            <FavoriteOutlinedIcon
+                                style={{ color: 'red' }}
+                                onClick={handleLike}
+                            />
+                        ) : (
+                            <FavoriteBorderOutlinedIcon onClick={handleLike} />
+                        )}
+                        {data && data.length} Likes
+                    </div>
+                    <div
+                        className="item"
+                        onClick={() => setCommentOpen(!commentOpen)}
+                    >
+                        <TextsmsOutlinedIcon />
+                        12 Comments
+                    </div>
+                    <div className="item">
+                        <ShareOutlinedIcon />
+                        Share
+                    </div>
+                </div>
+                {commentOpen && <Comments postId={post.id} />}
             </div>
-          </div>
-          <MoreHorizIcon />
         </div>
-        <div className="content">
-          <p>{post.desc}</p>
-          <img src={post.img} alt="" />
-        </div>
-        <div className="info">
-          <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 Likes
-          </div>
-          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-            <TextsmsOutlinedIcon />
-            12 Comments
-          </div>
-          <div className="item">
-            <ShareOutlinedIcon />
-            Share
-          </div>
-        </div>
-        {commentOpen && <Comments />}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Post;
